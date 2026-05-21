2e3e3dd7f9d2d5a6df9400193c396cb6e91a99ca
franking31 <frankpange157@icloud.com>
2026-05-21 13:31:27 +0200

    mofif et correction des bug

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -2,16 +2,16 @@ import { verifyAccess } from "../config/jwt.js";
 
 function authMiddleware(req, res, next) {
   try {
-    const authHeader = req.headers.authorization;
+    const token = req.cookies.accessToken;
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    if (!token) {
       return res.status(401).json({ message: "Not authenticated" });
     }
 
-    const token = authHeader.split(" ")[1];
     const decoded = verifyAccess(token);
 
     req.user = decoded;
+
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
