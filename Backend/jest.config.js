7d91bce18842d97d15fd31bad88d7c07eafc5d93
franking31 <frankpange157@icloud.com>
2026-05-21 13:03:05 +0200

    Ajout et modification des test

--- Backend/jest.config.js
+++ Backend/jest.config.js
@@ -1,10 +1,28 @@
+// jest.config.js
 export default {
-    testEnvironment: "node",
-    transform: {},
-    moduleFileExtensions: ["js", "json"],
-    roots: ["<rootDir>/tests"],
-    setupFilesAfterEnv: ["<rootDir>/tests/setup/test.env.js"],
-    moduleNameMapper: {
-        "^@prisma/client$": "<rootDir>/tests/setup/prisma-client.mock.js"
-    }
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  setupFiles: ["<rootDir>/tests/setup/test.env.js"],
+  testMatch: [
+    "**/tests/**/*.test.js",
+    "**/tests/**/*.test.JS",
+  ],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/config/env.js",
+    "!src/config/prisma.js",
+    "!src/prisma/**",
+    "!src/__mocks__/**",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 70,
+      functions: 70,
+      branches: 60,
+      statements: 70,
+    },
+  },
+  coverageReporters: ["text", "lcov", "html"],
 };
